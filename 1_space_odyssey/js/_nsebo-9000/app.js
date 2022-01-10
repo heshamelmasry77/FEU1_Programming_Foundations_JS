90b1d8c4159f0981e9a146b825746060a77df158
Uzochukwu Cecilia Oloh <fruklem16@gmail.com>
2022-01-10 12:24:08 +0100

    Update app.js

--- 1_space_odyssey/js/_nsebo-9000/app.js
+++ 1_space_odyssey/js/_nsebo-9000/app.js
@@ -139,10 +139,12 @@ console.log(" BROKEN SATELLITE::::::");
 
 /*
  uncomment these console logs and write your if else statement and use them to console log out the correct statement
-
- console.log("JetPack is enabled!!! off I go outside to fix the satellite");
- console.log("JetPack NOT enabled, please enable jetPack before doing a space walk");
  */
+
+console.log("JetPack is enabled!!! off I go outside to fix the satellite");
+console.log(
+  "JetPack NOT enabled, please enable jetPack before doing a space walk"
+);
 
 /*
  3. METEOR SHOWER::::::
